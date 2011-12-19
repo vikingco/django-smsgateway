a31a0e6b2c954af7c5483d6d6dfc296911e5fd99
Maarten Timmerman <maarten.timmerman@city-live.be>
2011-12-19 13:43:41 +0100

    import problem

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
-import smsgateway
 
 
 setup(
     name="django-smsgateway",
-    version=smsgateway.__version__,
+    version='0.0.1',
     url='https://github.com/citylive/django-smsgateway',
     license='BSD',
     description="SMS gateway for sending text messages",
