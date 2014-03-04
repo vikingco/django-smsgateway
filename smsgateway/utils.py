de3161d66ab0a5661d98ace04f5f0ae7c01062bf
Piotr Pawlaczek <info@pawlaczek.pl>
2014-03-04 13:21:14 +0100

    Use international MSISDN format according to SMPP protocol spec: 4.2.6.1.1

--- smsgateway/utils.py
+++ smsgateway/utils.py
@@ -9,12 +9,10 @@ def strspn(source, allowed):
     return u''.join(newchrs)
 
 def check_cell_phone_number(number):
-    cleaned_number = strspn(number, u'+0123456789')
-
-    if not u'+' in cleaned_number[:1]:
-        cleaned_number = u'+%s' % cleaned_number
-
-    return cleaned_number
+    cleaned_number = strspn(number, u'0123456789')
+    #if not u'+' in cleaned_number[:1]:
+    #    cleaned_number = u'+%s' % cleaned_number
+    return int(cleaned_number)
 
 def truncate_sms(text, max_length=160):
     if len(text) <= max_length:
