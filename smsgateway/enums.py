dbeb557ae21956c47db96540fedaeb063ef0f77b
Niels Van Och <niels.vanoch@gmail.com>
2017-02-20 16:03:51 +0100

    Add flake8 checks

--- smsgateway/enums.py
+++ smsgateway/enums.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy
 
 OPERATOR_UNKNOWN = 0
 OPERATOR_PROXIMUS = 1
@@ -6,11 +6,11 @@ OPERATOR_MOBISTAR = 2
 OPERATOR_BASE = 3
 OPERATOR_OTHER = 999
 OPERATOR_CHOICES = (
-    (OPERATOR_UNKNOWN, _(u'Unknown')),
+    (OPERATOR_UNKNOWN, ugettext_lazy(u'Unknown')),
     (OPERATOR_PROXIMUS, u'Proximus'),
     (OPERATOR_MOBISTAR, u'Mobistar'),
     (OPERATOR_BASE, u'Base'),
-    (OPERATOR_OTHER, _(u'Other')),
+    (OPERATOR_OTHER, ugettext_lazy(u'Other')),
 )
 
 GATEWAY_MOBILEWEB = 1
@@ -26,9 +26,9 @@ DIRECTION_BOTH = 2
 DIRECTION_INBOUND = 1
 DIRECTION_OUTBOUND = 0
 DIRECTION_CHOICES = (
-    (DIRECTION_BOTH, _(u'Both')),
-    (DIRECTION_INBOUND, _(u'Inbound')),
-    (DIRECTION_OUTBOUND, _(u'Outbound')),
+    (DIRECTION_BOTH, ugettext_lazy(u'Both')),
+    (DIRECTION_INBOUND, ugettext_lazy(u'Inbound')),
+    (DIRECTION_OUTBOUND, ugettext_lazy(u'Outbound')),
 )
 
 PRIORITY_HIGH = '1'
