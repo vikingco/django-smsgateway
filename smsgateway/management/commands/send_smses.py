bc15ecc67c60348b11a6b48b14336528252178c6
Jonas Steur <jonas.steur@vikingco.com>
2017-12-05 14:56:20 +0100

    Replace NoArgsCommand with BaseCommand (#32)
    NoArgsCommand is deprecated and removed in Django 1.10

--- smsgateway/management/commands/send_smses.py
+++ smsgateway/management/commands/send_smses.py
@@ -1,9 +1,8 @@
 from __future__ import absolute_import
 from logging import getLogger
-from optparse import make_option
 
 from django.conf import settings
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 from smsgateway.tasks import _send_smses as send_smses
 
@@ -12,29 +11,28 @@ LOCK_WAIT_TIMEOUT = getattr(settings, 'SMSES_LOCK_WAIT_TIMEOUT', -1)
 logger = getLogger(__name__)
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = 'Send SMSes in the queue. Defer the failed ones. Pass --send-deferred to retry those.'
 
-    option_list = NoArgsCommand.option_list + (
-        make_option(
+    def add_arguments(self, parser):
+        parser.add_argument(
             '--send-deferred',
             dest='send_deferred',
             action='store_true',
             help='Whether to send the deferred smses. Default is all non-deferred.'
         ),
-        make_option(
+        parser.add_argument(
             '--backend',
             dest='backend',
             action='store',
             help='Whether to use a certain backend.'
         ),
-        make_option(
+        parser.add_argument(
             '--limit',
             dest='limit',
             action='store',
             help='Limit the number of smses. Default is unlimited.'
         ),
-    )
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         send_smses(options['send_deferred'], options['backend'], options['limit'])
