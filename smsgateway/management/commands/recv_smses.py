da412b3fee84352af31c9b5a0ccc9965a0be53d6
JonasSteur <jonas.steur@vikingco.com>
2017-12-05 11:34:19 +0100

    Replace NoArgsCommand with BaseCommand NoArgsCommand is deprecated and removed in Django 1.10

--- smsgateway/management/commands/recv_smses.py
+++ smsgateway/management/commands/recv_smses.py
@@ -1,19 +1,18 @@
 from __future__ import absolute_import
-from django.core.management.base import NoArgsCommand
-from optparse import make_option
+from django.core.management.base import BaseCommand
 from smsgateway.tasks import recv_smses
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = 'Receive SMSes from the Redis queue.'
-    option_list = NoArgsCommand.option_list + (
-            make_option('--backend', help='Slug of the SMS backend to read from'),
-            make_option('--async',
-                        action='store_true',
-                        dest='async',
-                        default=False,
-                        help='Process the SMSes via asynchronously via Celery')
-            )
 
-    def handle_noargs(self, **options):
+    def add_arguments(self, parser):
+            parser.add_argument('--backend', help='Slug of the SMS backend to read from'),
+            parser.add_argument('--async',
+                                action='store_true',
+                                dest='async',
+                                default=False,
+                                help='Process the SMSes via asynchronously via Celery')
+
+    def handle(self, *args, **options):
         recv_smses(options['backend'], options['async'])
